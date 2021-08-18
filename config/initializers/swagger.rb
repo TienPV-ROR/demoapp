5fc330b88b183a714e3d680aa874291391a04bdd
TienPV <viettienn97@gmail.com>
2021-08-18 10:57:34 +0700

    fix document

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,2 +1,2 @@
-GrapeSwaggerRails.options.url      = '/swagger_doc.json'
-GrapeSwaggerRails.options.app_url  = 'http://swagger.wordnik.com'
+GrapeSwaggerRails.options.url      = '/api/v1/swagger_doc'
+GrapeSwaggerRails.options.app_url  = 'https://appname178.herokuapp.com'
